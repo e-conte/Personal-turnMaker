85a98f060802cd001df951b4ad6f77a66445d7f6
e-conte <160084751+e-conte@users.noreply.github.com>
2024-07-12 02:10:51 -0300

    "Generic index menu routes and static html Added"

--- user/user.py
+++ user/user.py
@@ -3,38 +3,58 @@ from common import common
 
 user_routes = Blueprint('user_routes', __name__)
 
-global business_info, business_color
-
+# imported variables
 business_info = common.business_info
 business_color = common.business_color
+business_menu = common.business_menu
+href_menu = common.href_menu
+
 
 @user_routes.route('/', methods=['GET', 'POST'])
 def index():
 
     _index_menu = {}
-    _href_menu = []
-    _index_menu = common.business_menu
-
-    for line in common.business_menu.values():
-        _href_menu.append(line.lower().replace(" ", "_"))
-           
-#    _href_menu = _href_menu.append(value.lower().replace(" ", "_"))
-
+    _index_menu = common.business_menu          
+    
     return render_template('index.html',
                            _index_menu=_index_menu,
-                           _href_menu=_href_menu,
+                           href_menu=href_menu,
                            business_info=business_info,
                            business_color=business_color,
-                           )
-    
-
-
-    
-@user_routes.route('/{}.format(business_info["first_line"])', methods=['GET', 'POST'])
+                           ) 
+   
+@user_routes.route('/' +f'{href_menu[0]}', methods=['GET', 'POST'])
 def first_line():
 
-    return render_template('index.html',
-                           _index_menu=common.business_menu,
+    return render_template(f'{business_menu.keys()[0]}' +'.html',
                            business_info=business_info,
-                           business_color=business_color
+                           business_color=business_color,
+                           href_menu=href_menu
+                           )
+
+@user_routes.route('/' +f'{href_menu[1]}', methods=['GET', 'POST'])
+def second_line():
+
+    return render_template(f'{business_menu.keys()[1]}'+'.html',
+                           business_info=business_info,
+                           business_color=business_color,
+                           href_menu=href_menu
+                           )
+
+@user_routes.route('/' +f'{href_menu[2]}', methods=['GET', 'POST'])
+def third_line():
+
+    return render_template(f'{business_menu.keys()[2]}'+'.html',
+                           business_info=business_info,
+                           business_color=business_color,
+                           href_menu=href_menu
+                           )
+
+@user_routes.route('/' +f'{href_menu[2]}', methods=['GET', 'POST'])
+def fourth_line():
+
+    return render_template(f'{business_menu.keys()[2]}'+'.html',
+                           business_info=business_info,
+                           business_color=business_color,
+                           href_menu=href_menu
                            )
