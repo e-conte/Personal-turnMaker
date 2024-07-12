9905c94ea2ec89371ff2c4c1a85bc8e4da2050a7
e-conte <160084751+e-conte@users.noreply.github.com>
2024-07-12 02:34:00 -0300

    href for menu in index is now a variable

--- common/common.py
+++ common/common.py
@@ -2,14 +2,16 @@ from flask import Blueprint
 
 common_variables = Blueprint('common_variables', __name__)
 
+ 
+
+
 #variables
 business_info = {
     "name" : "Business Name",
     "slogan" : "Business Slogan",
     "direction" : "Business Direction",
     "phone" : "Business Phone",
-    "email" : "Business Email",
-        
+    "email" : "Business Email",        
     }
 
 business_menu = {
@@ -17,15 +19,19 @@ business_menu = {
     "second_line" : "Consultar turno",
     "third_line" : "Nuestros servicios",
     "fourth_line" : "Tarifas",
-    
 }
-# De style.css #image_background + #menu_background 
+#processing information
+href_menu =[]
+for line in business_menu.values():
+    href_menu.append(line.lower().replace(" ", "_"))
+# This will create href_menu for index.html
+
+# style.css and #image_background and #menu_background should be changed/styled 
 
 colors = {"white" : '/static/white_style.css',
           "black" : '/static/black_style.css',
           "red": '/static/red_style.css',
           "custom": '/static/custom_style.css',
 }
-
 business_color = colors['red']  # this is the font color/style
 
